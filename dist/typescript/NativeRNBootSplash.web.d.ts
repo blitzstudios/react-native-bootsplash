72ddaff609084ba68bc2d533b2201754bf0624aa
Luke Herbold <luke@sleeper.app>
2026-01-09 12:26:13 -0800

    Add dist files for gitpkg.

---
+++ dist/typescript/NativeRNBootSplash.web.d.ts
@@ -0,0 +1,10 @@
+declare const _default: {
+    getConstants: () => {
+        darkModeEnabled: boolean;
+    };
+    hide: (fade: boolean) => Promise<void | undefined>;
+    isVisible: () => Promise<boolean>;
+    show: (_fade: boolean) => Promise<void>;
+};
+export default _default;
+//# sourceMappingURL=NativeRNBootSplash.web.d.ts.map
